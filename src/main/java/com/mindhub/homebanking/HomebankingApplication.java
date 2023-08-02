77c0af3b8b0840a271b86df0084108e22b198a14
gabycrem <gabymacre@gmail.com>
2023-08-02 18:33:38 -0300

    creacion del esqueleto

---
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -0,0 +1,13 @@
+package com.mindhub.homebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankingApplication.class, args);
+	}
+
+}
