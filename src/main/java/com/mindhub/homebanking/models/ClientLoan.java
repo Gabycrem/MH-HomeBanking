5952c64af4fb3d7ac11cdc1c10c84d513ca225f9
gabycrem <gabymacre@gmail.com>
2023-08-10 19:19:44 -0300

    Clase ClientLoan

---
+++ src/main/java/com/mindhub/homebanking/models/ClientLoan.java
@@ -0,0 +1,25 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.GenericGenerators;
+
+import javax.persistence.*;
+
+@Entity
+public class ClientLoan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+    private Double amount;
+    private Integer payments;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "loan_id")
+    private Loan loan;
+
+}
